d9351af36dfb1d5269c8719ca3e0fa2f9b136be6
AoiMorino <157398147+Aoi-Morino@users.noreply.github.com>
2025-02-17 23:44:23 +0900

    戦闘技能選択ウィンドウの整形

--- main.py
+++ main.py
@@ -66,27 +66,76 @@ class MainWindow(Qw.QMainWindow):
     log += self.tb_log.toPlainText()
     self.tb_log.setPlainText(log)
 
-  def show_new_window(self, checked):
-    self.w = AttackSelectWindow()
-    self.w.show()
-
 # * AttackSelectWindowクラス定義
 class AttackSelectWindow(Qw.QWidget):
 
   def __init__(self):
     super().__init__()
 
+    mainLayout = Qw.QVBoxLayout()
+
     # ウィンドウサイズの固定
-    self.setFixedSize(vfd.mainWindowSize[0] + 100, vfd.mainWindowSize[1] + 200)
+    self.setFixedSize(
+        vfd.AttackSelectWindowSize[0], vfd.AttackSelectWindowSize[1])
 
     # ウィンドウの位置をスクリーン中央に設定
     rect = Qc.QRect()
-    rect.setSize(Qc.QSize(vfd.mainWindowSize[0], vfd.mainWindowSize[1]))
+    rect.setSize(
+        Qc.QSize(vfd.AttackSelectWindowSize[0], vfd.AttackSelectWindowSize[1]))
     rect.moveCenter(app.primaryScreen().availableGeometry().center())
+    rect.translate(-400, 250)
     self.setGeometry(rect)
 
     # ウィンドウ名
     self.setWindowTitle('AttackSelect')
+
+    # メインレイアウトの設定
+    central_widget = Qw.QWidget(self)
+    mainLayout = Qw.QVBoxLayout(central_widget)  # 要素を垂直配置
+    mainLayout.setAlignment(Qc.Qt.AlignmentFlag.AlignTop)  # 上寄せ
+    mainLayout.setContentsMargins(15, 10, 10, 10)
+
+    # 上テキストの設定
+    selectTXT = Qw.QLabel('どう攻撃する？')  # 質問を表示する
+    mainLayout.addWidget(selectTXT)
+
+    # ラジオボタンを格納する画面表示上のコンテナ
+    rbLayout = Qw.QHBoxLayout()  # 要素を水平配置
+    rbLayout.setAlignment(Qc.Qt.AlignmentFlag.AlignLeft)  # 左寄せ
+    mainLayout.addLayout(rbLayout)
+
+    # ラジオボタンの生成と設定
+    self.attackChoices = Qw.QButtonGroup()  # 選択肢のグループ
+    for i, attacks in enumerate(vfd.attacks, 1):
+      rb = Qw.QRadioButton(self)  # 一つずつラジオボタンを作成する
+      rb.setText(attacks.name)
+      rbLayout.addWidget(rb)  # レイアウトに追加
+      self.attackChoices.addButton(rb, i)  # 選択グループに追加
+    # ラジオボタンがクリックされたら下の方のテクストの変化もさせる
+    self.attackChoices.buttonClicked.connect(self.rbClicked)
+    # 攻撃の説明文
+    self.attackExp = Qw.QLabel('')
+    mainLayout.addWidget(self.attackExp)
+
+    # ラベル
+    self.labelTXT = Qw.QLabel('', self)
+    mainLayout.addWidget(self.labelTXT)
+
+  def rbClicked(self):
+    rbId = self.attackChoices.checkedId() - 1
+    if 0 <= rbId < len(vfd.attacks):
+      TempTXT = f'成功率：{vfd.attacks[rbId].successRate}｜ダイス：{vfd.attacks[rbId].dice}d{vfd.attacks[rbId].damage}'
+      if vfd.attacks[rbId].fast:
+        TempTXT += ' | 速度：先行'
+      else:
+        TempTXT += ' | 速度：順行'
+      if vfd.attacks[rbId].indirect:
+        TempTXT += ' | 距離：遠距離'
+      else:
+        TempTXT += ' | 距離：近距離'
+    else:
+      TempTXT = 'errorText：異常な選択です。ゲームが破壊される可能性があります。選択しないでください。'
+    self.labelTXT.setText(TempTXT)
 
 
 # 本体
--- varFunc_def.py
+++ varFunc_def.py
@@ -9,22 +9,37 @@ def Dice_1D100():
   x = r.randint(1, 100)
   return x
 
-# ウィンドウサイズを指定（px単位）
+# メインウィンドウのサイズを指定（px単位）
 mainWindowSize = [640, 480]   # ウィンドウの横幅,縦幅
 
-# 攻撃手段を扱うクラスの定義
+# 戦闘技能ウィンドウのサイズを指定（px単位）
+AttackSelectWindowSize = [420, 120]
+
+# 戦闘技能を扱うクラスの定義
 class Attack():
   def __init__(self, name: str, successRate: int, dice: int, damage: int, fast: bool, indirect: bool):
-    self.name = name         # 攻撃技能
+    self.name = name         # 戦闘技能の名前
     self.successRate = successRate  # 成功率
     self.dice = dice         # ダイス数
     self.damage = damage  # 最大ダメージ量
     self.fast = fast  # 先制攻撃の制御
     self.indirect = indirect  # 遠距離攻撃の制御
 
-# 攻撃一覧のインスタンスの生成
-Attacks = [Attack('こぶし（パンチ）', 50, 1, 3, False, True),
-           Attack('キック', 25, 1, 6, False, True),
+# 戦闘技能一覧のインスタンスの生成
+attacks = [Attack('こぶし（パンチ）', 50, 1, 3, False, False),
+           Attack('キック', 25, 1, 6, False, False),
            Attack('頭突き', 30, 1, 4, True, False),
            Attack('投擲', 25, 1, 4, False, True),
            Attack('拳銃', 20, 1, 8, True, True),]
+
+# ?以下、用意したけど使う予定はなし（時間があれば追加予定）
+# 特殊攻撃を扱うクラスの定義
+class SpecialAttack():
+  def __init__(self, name: str, successRate: int, effect: str):
+    self.name = name
+    self.successRate = successRate
+    self.effect = effect
+
+# 特殊攻撃一覧のインスタンスの生成
+SpecialAttacks = [SpecialAttack('マーシャルアーツ', 30, '素手での攻撃（こぶし、キック、組みつき、頭突き）のダメージ２倍'),
+                  SpecialAttack('頭狙い', 50, '武器での攻撃（投擲、拳銃）のダメージ２倍、失敗すると外れる')]
